70e8fec4b2a8c870d2b96741df0e871918865804
kanishkasharma17 <ckanisharma0000@gmail.com>
2026-07-01 13:52:03 +0530

    feat  : add request validation middleware

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,13 +1,28 @@
 const express = require("express");
 
 const router = express.Router();
+const validate = require("../middleware/validationMiddleware");
 
+const {
+    registerValidator,
+    loginValidator
+} = require("../validators/authValidator");
 const {
     registerUser,
     loginUser
 } = require("../controllers/authController");
 
-router.post("/register", registerUser);
-router.post("/login", loginUser);
+router.post(
+    "/register", 
+    registerValidator,
+    validate,
+    registerUser);
+
+router.post(
+    "/login", 
+    loginValidator,
+    validate,
+    loginUser
+);
 
 module.exports = router;
