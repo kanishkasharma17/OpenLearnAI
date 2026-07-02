c485bb2f4d05a607702ed073612f6d01d94eed18
kanishkasharma17 <ckanisharma0000@gmail.com>
2026-07-02 16:59:51 +0530

    Prepare ML services for deployment

--- ml/app.py
+++ ml/app.py
@@ -81,9 +81,5 @@ def predict():
             "error": str(e)
         }), 500
 
-if __name__ == "__main__":
-    app.run(
-        debug=True,
-        port=5000
-    )
+app.run(host="0.0.0.0", port=5000)
 
